b2ca65b310dfdf76e461d083586082f7512ec989
Abhilash Reddy <abhilashreddysh.99@gmail.com>
2023-05-19 06:38:52 +0530

    initial commit

---
+++ docs/home.md
@@ -0,0 +1,17 @@
+---
+title: Abhi's Personal Documentation
+slug: /
+sidebar_position: 1
+sidebar_label: "Home"
+id: Home
+---
+
+<!-- # Welcome to Abhi's Personal Documentation: Unlocking the Power of Learning -->
+
+![Home Archives Image](/img/home.jpg)
+
+Join me on an exciting journey as I explore the ever-evolving world of technology. Discover `coding tips`, **emerging trends**, and `practical insights` to enhance your skills. From programming languages to cutting-edge tools and frameworks, _dive deep_ into the realm of technology and unleash your full potential. Learn, experiment, and grow as we navigate the exciting landscape of innovation together.
+
+Get ready to embark on a **transformative learning experience** that will empower you to thrive in the digital age. Let's `dive into the world of technology` and embrace the endless possibilities it holds.
+
+**_Happy exploring and learning!_**
---
+++ docusaurus.config.js
@@ -0,0 +1,182 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "ASH Wiki",
+  tagline: "My Wiki in my Words",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://abhilashreddysh.github.io/",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/wiki",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "abhilashreddysh", // Usually your GitHub org/user name.
+  projectName: "wiki", // Usually your repo name.
+
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/abhilashreddysh/wiki",
+        },
+        blog: false,
+        // {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        // },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+  themes: [
+    // ... Your other themes.
+    [
+      // @ts-ignore
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      // @ts-ignore
+      ({
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        language: ["en"],
+        highlightSearchTermsOnTargetPage: true,
+        explicitSearchResultPath: true,
+      }),
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      metadata: [{ name: "keywords", content: "abhi,wiki,docs,documentation" }],
+      // Replace with your project's social card
+      image: "img/logo.png",
+      announcementBar: {
+        id: "announcement-bar",
+        content: "Documenting this as I learn stuff :)",
+        backgroundColor: "inherit",
+        textColor: "inherit",
+        isCloseable: true,
+      },
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: "ASH Wiki",
+        logo: {
+          alt: "Abhi Logo",
+          src: "img/logo.png",
+        },
+        items: [
+          // {
+          //   type: "docSidebar",
+          //   sidebarId: "tutorialSidebar",
+          //   position: "left",
+          //   label: "Tutorial",
+          // },
+          // { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/abhilashreddysh",
+            label: "GitHub",
+            position: "right",
+            className: "header-github-link",
+          },
+          {
+            type: "search",
+            position: "left",
+          },
+        ],
+        hideOnScroll: true,
+      },
+      footer: {
+        style: "dark",
+        links: [
+          // {
+          //   title: "Docs",
+          //   items: [
+          //     {
+          //       label: "Tutorial",
+          //       to: "/docs/intro",
+          //     },
+          //   ],
+          // },
+          {
+            title: "Social",
+            items: [
+              // {
+              //   label: "Stack Overflow",
+              //   href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              // },
+              // {
+              //   label: "Discord",
+              //   href: "https://discordapp.com/invite/docusaurus",
+              // },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/shabhilashreddy",
+              },
+              {
+                label: "Github",
+                href: "https://github.com/abhilashreddysh",
+              },
+              {
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/in/sh-abhilash ",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
+              {
+                label: "GitHub Repository",
+                href: "https://github.com/abhilashreddysh/documentation",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Abhilash Reddy.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
