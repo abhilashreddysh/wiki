c00fca575c9eaaffdba8925227ccd7f5c0f09dab
Abhilash Reddy <abhilashreddysh.99@gmail.com>
2023-05-25 08:48:01 +0530

    remove algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,26 +62,17 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      algolia: {
-        // The application ID provided by Algolia
-        appId: "IMCVZQZ3KP",
-        // Public API key: it is safe to commit it
-        apiKey: "71d35c4b218e673618e38b53958ac7b2",
-        indexName: "wiki",
-        // Optional: see doc section below
-        contextualSearch: true,
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        // externalUrlRegex: 'external\\.com|domain\\.com',
-        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-        replaceSearchResultPathname: {
-          from: "/docs/", // or as RegExp: /\/docs\//
-          to: "/",
-        },
-        // Optional: Algolia search parameters
-        // searchParameters: {},
-        // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: "search",
-      },
+      // algolia: {
+      //   // The application ID provided by Algolia
+      //   appId: "IMCVZQZ3KP",
+      //   // Public API key: it is safe to commit it
+      //   apiKey: "920cb119f0922967e933c32b8fc4467f",
+      //   indexName: "wiki",
+      //   // Optional: see doc section below
+      //   contextualSearch: true,
+      //   placeholder: "Search the Docs",
+      //   searchPagePath: "search",
+      // },
       metadata: [{ name: "keywords", content: "abhi,wiki,docs,wiki" }],
       // Replace with your project's social card
       image: "img/logo.png",
