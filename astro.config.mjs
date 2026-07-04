996092776596994b091722dd045091c5f7f8e3bc
Abhilash Reddy <116741403+abhilashreddysh@users.noreply.github.com>
2026-07-04 18:38:59 +0530

    Migrated docs from mkdocs to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightThemeNova from 'starlight-theme-nova';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,13 @@ export default defineConfig({
       logo: {
         src: "./src/assets/book.svg",
       },
+      plugins: [
+        starlightThemeNova({
+          nav: [
+            { label: 'Github', href: 'https://github.com/abhilashreddysh' },
+          ],
+        })
+      ],
       social: [
         {
           icon: "github",
@@ -19,24 +27,38 @@ export default defineConfig({
       ],
       sidebar: [
         {
-          label: "Homelab Project",
-          autogenerate: { directory: "homelab" },
+          label: "Homelab",
+          items: [{ autogenerate: { directory: 'homelab' } }],
+        },
+        {
+          label: "Notes",
+          items: [
+            { label: "Overview", link: "/notes/"},
+            { 
+              label: "Guides",
+              collapsed: true,
+              items: [{autogenerate: { directory: 'notes/guides'}}]
+            },
+            { 
+              label: "How-To",
+              collapsed: true,
+              items: [{autogenerate: { directory: 'notes/how-to'}}]
+            },
+            { 
+              label: "Cheatsheets",
+              collapsed: true,
+              items: [{autogenerate: { directory: 'notes/cheatsheets'}}]
+            },
+            { 
+              label: "References",
+              collapsed: true,
+              items: [{autogenerate: { directory: 'notes/reference'}}]
+            },
+          ],
         },
         {
           label: "Projects",
-          autogenerate: { directory: "projects" },
-        },
-        {
-          label: "Guides",
-          autogenerate: { directory: "guides" },
-        },
-        {
-          label: "Setup",
-          autogenerate: { directory: "setup" },
-        },
-        {
-          label: "References",
-          autogenerate: { directory: "reference" },
+          items: [{ autogenerate: { directory: 'projects' } }],
         },
       ],
       customCss: ["./src/styles/global.css"],
