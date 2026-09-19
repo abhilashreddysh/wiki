3465baa5ca1122556137c5c00d5b2e051433d6a0
Abhilash Reddy <116741403+abhilashreddysh@users.noreply.github.com>
2026-09-19 20:00:07 +0530

    Update docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,29 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import starlightThemeNova from 'starlight-theme-nova';
+import starlightThemeNova from "starlight-theme-nova";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       title: "Abhi's Wiki",
+
       logo: {
         src: "./src/assets/book.svg",
       },
+
       plugins: [
         starlightThemeNova({
           nav: [
-            { label: 'Github', href: 'https://github.com/abhilashreddysh' },
+            {
+              label: "GitHub",
+              href: "https://github.com/abhilashreddysh",
+            },
           ],
-        })
+        }),
       ],
+
       social: [
         {
           icon: "github",
@@ -25,42 +31,85 @@ export default defineConfig({
           href: "https://github.com/abhilashreddysh/wiki",
         },
       ],
+
       sidebar: [
         {
-          label: "Homelab",
-          items: [{ autogenerate: { directory: 'homelab' } }],
-        },
-        {
-          label: "Notes",
+          label: "Self-Hosted",
           items: [
-            { label: "Overview", link: "/notes/"},
-            { 
-              label: "Guides",
-              collapsed: true,
-              items: [{autogenerate: { directory: 'notes/guides'}}]
-            },
-            { 
-              label: "How-To",
-              collapsed: true,
-              items: [{autogenerate: { directory: 'notes/how-to'}}]
-            },
-            { 
-              label: "Cheatsheets",
-              collapsed: true,
-              items: [{autogenerate: { directory: 'notes/cheatsheets'}}]
-            },
-            { 
-              label: "References",
-              collapsed: true,
-              items: [{autogenerate: { directory: 'notes/reference'}}]
+            {
+              autogenerate: {
+                directory: "self-hosted",
+              },
             },
           ],
         },
+
+        {
+          label: "Notes",
+          items: [
+            {
+              label: "Overview",
+              link: "/notes/",
+            },
+            {
+              label: "Guides",
+              collapsed: true,
+              items: [
+                {
+                  autogenerate: {
+                    directory: "notes/guides",
+                  },
+                },
+              ],
+            },
+            {
+              label: "How-To",
+              collapsed: true,
+              items: [
+                {
+                  autogenerate: {
+                    directory: "notes/how-to",
+                  },
+                },
+              ],
+            },
+            {
+              label: "Cheatsheets",
+              collapsed: true,
+              items: [
+                {
+                  autogenerate: {
+                    directory: "notes/cheatsheets",
+                  },
+                },
+              ],
+            },
+            {
+              label: "Reference",
+              collapsed: true,
+              items: [
+                {
+                  autogenerate: {
+                    directory: "notes/reference",
+                  },
+                },
+              ],
+            },
+          ],
+        },
+
         {
           label: "Projects",
-          items: [{ autogenerate: { directory: 'projects' } }],
+          items: [
+            {
+              autogenerate: {
+                directory: "projects",
+              },
+            },
+          ],
         },
       ],
+
       customCss: ["./src/styles/global.css"],
     }),
   ],
